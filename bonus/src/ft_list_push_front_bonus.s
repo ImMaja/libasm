52c4ad3e365430281f22ced88d2c4e19113f403a
Maja <guill.peiffer@gmail.com>
2026-06-03 13:52:30 +0200

    .

---
+++ bonus/src/ft_list_push_front_bonus.s
@@ -0,0 +1,18 @@
+global ft_list_push_front
+
+section .text
+; void ft_list_push_front(t_list **begin_list, void *data);
+; Arguments:
+;	rdi = begin_list
+;	rsi = data
+;
+; Return:
+;	void
+;
+; Registers:
+;
+; Calls:
+;   _check_base
+
+ft_list_push_front:
+
--- bonus/tests/tests_bonus.h
+++ bonus/tests/tests_bonus.h
@@ -6,5 +6,6 @@ void	print_test_title(const char *title);
 
 /** tests functions definitions */
 int	test_ft_atoi_base(void);
+int	test_ft_list_push_front(void);
 
 #endif /** TESTS_BONUS_H */
